d4ed58a2a712acc1583834feebb7b3bfa8d3ea34
saint <saint@kali.local>
2020-10-18 13:48:31 +0200

    Hyperscript fixad TW 1.3

--- js/views/SummaryView.js
+++ js/views/SummaryView.js
@@ -1,14 +1,3 @@
 function SummaryView(props){
-   return h("div",{title:"summary"},"Dinner for",h("span",{title:"nr.guests",},2),"guests"
-
-
-
-
-
-
-
-
-
-
-   );
+   return h("div",{title:"summary"},"Dinner for ",h("span",{title:"nr.guests",},props.guests)," guests" );
 }
