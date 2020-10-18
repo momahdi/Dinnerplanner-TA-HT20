228cd75d6f1c229e500e5cdf89e251e56184bf33
saint <saint@kali.local>
2020-10-18 13:23:20 +0200

    Basic DOM API TW 1.3

--- js/views/SummaryView.js
+++ js/views/SummaryView.js
@@ -1,4 +1,11 @@
 function SummaryView(props){
-   const {guests}=props;
-   return document.createTextNode(guests);
+    const div =document.createElement("div");
+    div.setAttribute("title","summary");
+    div.append("Dinner for");
+    const span = document.createElement("span");
+    span.setAttribute("title", "nr. guests")
+    span.append(2)
+    div.append(span);
+    div.append("guests");
+    return div;
 }
