6822cc0628f8c3ad164ec1a4f9299047a69337dc
saint <saint@kali.local>
2020-10-18 13:31:12 +0200

    Hyperscript TW 1.3

--- js/views/SummaryView.js
+++ js/views/SummaryView.js
@@ -1,11 +1,14 @@
 function SummaryView(props){
-    const div =document.createElement("div");
-    div.setAttribute("title","summary");
-    div.append("Dinner for");
-    const span = document.createElement("span");
-    span.setAttribute("title", "nr. guests")
-    span.append(2)
-    div.append(span);
-    div.append("guests");
-    return div;
+   return h("div",{title:"summary"},"Dinner for",h("span",{title:"nr.guests",},2),"guests"
+
+
+
+
+
+
+
+
+
+
+   );
 }
