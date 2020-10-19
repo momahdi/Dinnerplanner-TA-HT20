ab1bf581033e9659ac1cd54b81a83c8244544a76
saint <saint@kali.local>
2020-10-19 16:37:12 +0200

    Completed  TW 1.5

--- js/views/SidebarView.js
+++ js/views/SidebarView.js
@@ -1,6 +1,6 @@
 const SidebarView= ({guests,setGuests})=>
     <div>
-        <button onClick={()=>setGuests(guests-1)}>-</button>
+        <button onClick={()=>setGuests(guests-1)} disabled={guests ===1 } >-</button>
         {guests}
         <button onClick={()=>setGuests(guests+ 1)}>+</button>
     </div>
