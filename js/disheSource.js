f7da3c4704e97b45097bde8e9e7361ff82eb2429
saint <saint@kali.local>
2020-10-21 22:47:43 +0200

    completed TW 2.2

---
+++ js/disheSource.js
@@ -0,0 +1,38 @@
+const  DishSource={
+    apiCall(Params) {
+     return fetch(BASE_URL+Params,{
+             "method": "GET",
+             "headers": {
+          'X-Mashape-Key' : API_KEY
+          }
+     }).then(response => response.json())          
+
+ },
+
+ searchDishes({type, query}) { 
+     if (!type){
+         type="";
+     }
+     if(!query){
+         query="";
+     }
+     return this.apiCall("recipes/search?type="+type+"&query="+query)  
+     .then(data=>data.results);       
+  },
+
+  getDishDetails(id){
+      if(!id){
+          id="";
+      }
+    return this.apiCall("recipes/"+id+"/information")  
+    .then(data=>data);    
+
+  }
+
+
+
+
+
+
+
+ }; 
